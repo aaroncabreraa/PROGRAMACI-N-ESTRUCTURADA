be494a4897a2081a69bbe1bf724cc076354ab088
aaroncabreraa <60860626+aaroncabreraa@users.noreply.github.com>
2020-06-01 19:03:56 -0500

    Add files via upload

---
+++ Practica#1_Monday_1918753/Practica #1_Monday_1918753.cpp
@@ -0,0 +1,13 @@
+//AARON BUENO CABRERA 1918753
+/*Program a Hello World*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	printf("\n\tHola mundo.\n\n");
+	
+	system("pause");
+	return 0;
+}
